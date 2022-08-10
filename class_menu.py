b607db1136ac1cb1417e9e0f02dfdf4a4068294d
EwersonBrandina <106076391+EwersonBrandina@users.noreply.github.com>
2022-08-10 18:38:08 -0300

    Update class_menu.py

--- class_menu.py
+++ class_menu.py
@@ -10,16 +10,18 @@ class Menu():
                         f'0 - Sair\n'
                         f': ')
             if entrada == '1':
-                #cpf_cnpj = input('CPF: ')
-                #nome = input('Nome: ')
-                #dataNasc = input('Data Nasc.: ')
-                #telefone = input('Tel.: ')
+                cpf_cnpj = input('CPF: ')
+                nome = input('Nome: ')
+                dataNasc = input('Data Nasc.: ')
+                telefone = input('Tel.: ')
+                senha = input('Senha.: ')
 
-                cpf_cnpj = "123.456.789-03"
-                nome = "Ewerson"
-                dataNasc = "1993/03/17"
-                telefone = "(035)9-9233-7155"
-                cadastro.cadastros(cpf_cnpj, nome, dataNasc, telefone)
+                #cpf_cnpj = "123.456.789-03"
+                #nome = "Ewerson"
+                #dataNasc = "1993/03/17"
+                #telefone = "(035)9-9233-7155"
+                #senha = "123456789"
+                cadastro.cadastros(cpf_cnpj, nome, dataNasc, telefone, senha)
             elif entrada == '2':
                 cadastro.listar_usuarios()
             elif entrada == '0':
