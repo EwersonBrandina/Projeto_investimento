dea707e42e967080f1bc7d9c3d61b3fe1e96353c
EwersonBrandina <106076391+EwersonBrandina@users.noreply.github.com>
2022-08-10 18:38:26 -0300

    Update class_cadastro.py

--- class_cadastro.py
+++ class_cadastro.py
@@ -1,40 +1,30 @@
+from tkinter import messagebox
 import mysql.connector
 from class_usuario import*
-from class_movimentacao import*
+class Cadastro:
 
-class bancoDados:
+    def __init__(self):
+        self.conexao = mysql.connector.connect(host='localhost', user='root', password='92337155Thule03@1', database='investimentos')
+        self.mycursor = self.conexao.cursor() 
 
-    def __init__(self, msg = 'Segue a Lista Abaixo:'):
-        self.msg = msg
-        self.conexao = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='q1w2e3',
-            database='investimentos'
-        )
-        self.meu_cursor = self.conexao.cursor() 
-
-    def cadastros(self, cpf_cnpj, nome, dataNasc, telefone):
+    def cadastro(self,cpf_cnpj, nome, dataNasc, telefone, senha):
+        cpf_cnpj = cpf_cnpj
+        nome = nome
+        dataNasc = dataNasc
+        telefone = telefone
+        senha = senha
         comando_sql = f'select * from Usuarios'
-        self.meu_cursor.execute(comando_sql)
-        lista = self.meu_cursor.fetchall()
+        self.mycursor.execute(comando_sql)
+        lista = self.mycursor.fetchall()
         contador=0
         for i in lista:
             if cpf_cnpj == i[0]:
                 contador=1
-                print('CPF/CNPJ já Cadastrado')
+                messagebox.showinfo('', 'User já cadastrado')
                 break
         if contador == 0:
-            obj_cadastros = Usuario(cpf_cnpj, nome, dataNasc, telefone)
-            comando_sql = f"insert into Usuarios (cpf_cnpj, nome, dataNasc, telefone) value ('{obj_cadastros.cpf_cnpj}','{obj_cadastros.nome}','{obj_cadastros.dataNasc}','{obj_cadastros.telefone}')"
-            self.meu_cursor.execute(comando_sql)
+            obj_cadastros = Usuario(cpf_cnpj, nome, dataNasc, telefone, senha)
+            comando_sql = f"insert into Usuarios (cpf_cnpj, nome, dataNasc, telefone, senha) value ('{obj_cadastros.cpf_cnpj}','{obj_cadastros.nome}','{obj_cadastros.dataNasc}','{obj_cadastros.telefone}', '{obj_cadastros.senha}')"
+            self.mycursor.execute(comando_sql)
             self.conexao.commit()
-
-    def listar_usuarios(self):
-        comando_sql = f'select * from Usuarios'
-        self.meu_cursor.execute(comando_sql)
-        lista = self.meu_cursor.fetchall()
-        for i in lista:
-            self.msg += f'\nCpf/CNPJ: {i[0]}, Nome: {i[1]}, Data Nasc.: {i[2]}, Tel.:{i[3]}'
-        print(self.msg)
-        self.msg='Segue a Lista Abaixo'
+            messagebox.showinfo('', 'User foi cadastrado')
